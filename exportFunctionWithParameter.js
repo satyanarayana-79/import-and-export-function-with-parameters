95fcdaf77da8e78e2faf7d0e0b032004e662181e
satyanarayana <satyanarayanatadala289@gmail.com>
2023-08-10 05:53:34 +0000

    import-export-function-parameters

---
+++ exportFunctionWithParameter.js
@@ -0,0 +1,2 @@
+let export_function_with_parameters = (a) => a ** 2;
+module.exports = export_function_with_parameters;
---
+++ importFunctionWithParameter.js
@@ -0,0 +1,1 @@
+let import_function_with_parameters = require("./exportFunctionWithParameter");
